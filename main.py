4696af9b3de008fcb2fa336cf73fa3b1caf0a160
D4viMartinsLima <martinslimafkc@gmail.com>
2025-09-25 17:53:37 -0300

    Border implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.title("My Snake Game")
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,6 +31,14 @@ while gameIsOn:
     #Detecting food
     if  snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
+
+    #Wall detection
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        gameIsOn = False
+        scoreboard.game_over()
+
 
 
 
